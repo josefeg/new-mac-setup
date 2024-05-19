b875056aff74327bc3537bf6cab4a069d007f562
Josef Galea <josefeg@gmail.com>
2024-05-19 07:24:57 +0200

    Create temp directory and delete after use

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,33 +3,36 @@
 set -euo pipefail
 IFS=$'\n\t'
 
-mkdir temp
-
 xcode-select --install
 echo -n "Installing command line tools"
-until $(xcode-select --print-path &> /dev/null); do
+until eval "xcode-select --print-path &> /dev/null"; do
   sleep 5;
   echo -n "."
 done
 echo
 
-curl https://bootstrap.pypa.io/get-pip.py -o ./temp/get-pip.py
-python3 ./temp/get-pip.py
+TEMP_DIR=$(mktemp -d)
+
+curl https://bootstrap.pypa.io/get-pip.py -o "$TEMP_DIR/get-pip.py"
+python3 "$TEMP_DIR/get-pip.py"
 
 sudo pip3 install ansible
 
-git clone https://github.com/josefeg/new-mac-setup.git ./temp/new-mac-setup
+git clone https://github.com/josefeg/new-mac-setup.git "$TEMP_DIR/new-mac-setup"
 
-ansible-galaxy install -r ./temp/new-mac-setup/ansible_requirements.yaml
+ansible-galaxy install -r "$TEMP_DIR/new-mac-setup/ansible_requirements.yaml"
 
-ansible-playbook -i "localhost," -c local ./temp/new-mac-setup/ansible_playbook.yaml --ask-become-pass
+ansible-playbook -i "localhost," -c local "$TEMP_DIR/new-mac-setup/ansible_playbook.yaml" --ask-become-pass
 
 export HOMEBREW_BIN_PATH=/opt/homebrew/bin
 export PATH=$PATH:$HOMEBREW_BIN_PATH
 
-export BW_SESSION=$(bw login --raw)
+BW_SESSION=$(bw login --raw)
+export BW_SESSION
 
 chezmoi init https://github.com/josefeg/dotfiles.git
 chezmoi apply --force
 
 bw logout
+
+rm -rf "$TEMP_DIR"
